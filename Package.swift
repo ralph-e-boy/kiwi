9791b8a5bdf867f30648c9a2abe92192eb15f314
ralph <cobble-immured-69@icloud.com>
2026-04-07 02:22:44 -0400

    add docc plugin

--- Package.swift
+++ Package.swift
@@ -13,6 +13,9 @@ let package = Package(
     products: [
         .library(name: "KiwiSolver", targets: ["KiwiSolver"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+    ],
     targets: [
         .target(
             name: "CxxKiwi",
